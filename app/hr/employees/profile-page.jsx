f57a8808c874f2d6368b65a55979f2e4af93506e
Montaha GUEDHAMI <127242308+montahaguedhami@users.noreply.github.com>
2026-04-28 11:46:31 +0100

    Delete app directory

--- app/hr/employees/profile-page.jsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import { EmployeeProfileRoutePage } from "@/components/employees/employee-profile-route-page"
-
-export default function HREmployeeProfilePage() {
-  return <EmployeeProfileRoutePage rolePrefix="/hr" accentClass="text-orange-500" />
-}
